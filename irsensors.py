4ca1d8949a27a9e73503bd8015716da79b8aae6a
RobertKajnak <hezirisz@gmail.com>
2019-01-23 19:29:53 +0100

    improved doc. Can now adapt to stuck sensors (untested)

--- irsensors.py
+++ irsensors.py
@@ -1,12 +1,22 @@
+from collections import deque
+
 class Sensors:
     out = 0
     close = 0.5
     collision = 1
     
     def __init__(self,robot, is_simulation):
+        """Handles IR sensor for the robot. 
+        Sensor order: back, from left to right, front from right to left
+            Args:
+                robot: robot returned from simulation.py or hardware.py
+                is_simulation: True for V-REP, False for physical Robobo
+        """
         self.rob = robot
+        self.mem_length = 3
         self.is_simulation = is_simulation
         if not is_simulation:
+            self.prev_vals =[deque([0]*self.mem_length,self.mem_length) for i in range(8)]
             self.touching = 1500
             self.thresholds =  [
                 [25,200,self.touching],
@@ -26,20 +36,44 @@ class Sensors:
                                 [[0.05,0.15] for i in range(2)]
                 
     def set_sensor_baseline(self):
+        """Set current sensors values+10% as the comparison baseline for trigger"""
         cont = self.continuous()
         for idx,val in enumerate(cont):
             self.thresholds[idx][0] = val*1.1
     
     def continuous(self):
-        return self.rob.read_irs()
+        """
+            Returns the values of the raw data from the sensors.
+            if not is_simulation also adapts to blocked sensors
+            Returns:
+                in simulation: False if no detection, and distance from 0->0.2 in meters
+                    e.g. [False,False,0.1,0.123 ...]
+                physical: 0->10k+ inversely proportional to distance
+                    [24,32,130,100,...]
+        """
+        sens = self.rob.read_irs()
+        if not self.is_simulation:
+            for idx,val in enumerate(sens):
+                self.prev_vals[idx].append(val)
+                identical = True
+                for j in range(self.mem_length-1):
+                    if self.prev_vals[idx][j]!=self.prev_vals[idx][j+1]:
+                        identical = False
+                        break
+                if identical:
+                    self.thresholds[idx][0] = self.prev_vals[idx][0]
+            
+        return sens
     
     def discrete(self):
+        """Calculated discretized values to 3 states: far, close, collision
+        """
         if self.is_simulation:
             disc=[]
             sens = self.continuous()
             for idx,val in enumerate(sens):
                 if val==False or val>=self.thresholds[idx][1]:
-                    disc.append(self.out)
+                    disc.append(self.far)
                 elif val>=self.thresholds[idx][0]:
                     disc.append(self.close)
                 else:
@@ -50,7 +84,7 @@ class Sensors:
             sens = self.continuous()
             for idx,val in enumerate(sens):
                 if val<=self.thresholds[idx][0]:
-                    disc.append(self.out)
+                    disc.append(self.far)
                 elif val<=self.thresholds[idx][1]:
                     disc.append(self.close)
                 else:
@@ -58,17 +92,25 @@ class Sensors:
         return disc
         
     def one_hot(self):
+        """ Deprecated. [1 0] for far, [0 1] for close, for each sensor
+        """
         disc = self.discrete()
         hot = []
         hit=False
         for val in disc:
-            hot+= [int(val==self.out),
+            hot+= [int(val==self.far),
                    int(val==self.close)]
             if val==self.collision:
                 hit=True
         return hot,hit
     
     def binary(self):
+        """ Reurns the binary form, i.e. is the object close or not
+            Returns:
+                hot: [1, 1, 0, ...] if the first two sensors detect an object.
+                    far=> 0; close or collision=>1
+                hit: True if any of the sensors detect collsion, otherwise false
+        """
         disc = self.discrete()
         hot = []
         hit = False
@@ -79,10 +121,14 @@ class Sensors:
         return hot, hit
     
     def strings(self):
+        """ The values converted to strings
+        Returns:
+            e.g. ['far','far','close','collision'...]
+        """
         disc = self.discrete()
         def toString(val):
-            if val==self.out:
-                return 'out'
+            if val==self.far:
+                return 'far'
             elif val==self.close:
                 return 'close'
             else: 
