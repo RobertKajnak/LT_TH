7fbb972d69103d7f82a1f2a86ef9e26b62d17fce
RobertKajnak <hezirisz@gmail.com>
2019-01-29 19:24:29 +0100

    Can sensor data can now be overriden

--- irsensors.py
+++ irsensors.py
@@ -35,14 +35,15 @@ class Sensors:
                                 [[0.01,0.15] for i in range(1)] +\
                                 [[0.05,0.15] for i in range(2)]
                 
-    def set_sensor_baseline(self):
+    def set_sensor_baseline(self, sensor_data=None):
         """Set current sensors values+10% as the comparison baseline for trigger"""
-        cont = self.continuous()
+        cont = self.continuous(sensor_data)
         for idx,val in enumerate(cont):
             self.thresholds[idx][0] = val*1.1
     
-    def continuous(self):
-        """
+    def continuous(self, sensor_data=None):
+        """ Args:
+                sensor_data: if None is specified rob.read_irs() is used
             Returns the values of the raw data from the sensors.
             if not is_simulation also adapts to blocked sensors
             Returns:
@@ -51,7 +52,9 @@ class Sensors:
                 physical: 0->10k+ inversely proportional to distance
                     [24,32,130,100,...]
         """
-        sens = self.rob.read_irs()
+        if sensor_data is None:
+            sens = self.rob.read_irs()
+            
         if not self.is_simulation:
             for idx,val in enumerate(sens):
                 self.prev_vals[idx].append(val)
@@ -61,16 +64,16 @@ class Sensors:
                         identical = False
                         break
                 if identical:
-                    self.thresholds[idx][0] = self.prev_vals[idx][0]
+                    self.thresholds[idx][0] = self.prev_vals[idx][0] *1.5
             
         return sens
     
-    def discrete(self):
+    def discrete(self, sensor_data=None):
         """Calculated discretized values to 3 states: far, close, collision
         """
         if self.is_simulation:
             disc=[]
-            sens = self.continuous()
+            sens = self.continuous(sensor_data)
             for idx,val in enumerate(sens):
                 if val==False or val>=self.thresholds[idx][1]:
                     disc.append(self.far)
@@ -81,7 +84,7 @@ class Sensors:
                         
         else:
             disc=[]
-            sens = self.continuous()
+            sens = self.continuous(sensor_data)
             for idx,val in enumerate(sens):
                 if val<=self.thresholds[idx][0]:
                     disc.append(self.far)
@@ -91,7 +94,7 @@ class Sensors:
                     disc.append(self.collision)
         return disc
         
-    def one_hot(self):
+    def one_hot(self, sensor_data=None):
         """ Deprecated. [1 0] for far, [0 1] for close, for each sensor
         """
         disc = self.discrete()
@@ -104,14 +107,14 @@ class Sensors:
                 hit=True
         return hot,hit
     
-    def binary(self):
+    def binary(self, sensor_data=None):
         """ Reurns the binary form, i.e. is the object close or not
             Returns:
                 hot: [1, 1, 0, ...] if the first two sensors detect an object.
                     far=> 0; close or collision=>1
                 hit: True if any of the sensors detect collsion, otherwise false
         """
-        disc = self.discrete()
+        disc = self.discrete(sensor_data)
         hot = []
         hit = False
         for val in disc:
@@ -120,12 +123,12 @@ class Sensors:
                 hit=True
         return hot, hit
     
-    def strings(self):
+    def strings(self, sensor_data=None):
         """ The values converted to strings
         Returns:
             e.g. ['far','far','close','collision'...]
         """
-        disc = self.discrete()
+        disc = self.discrete(sensor_data)
         def toString(val):
             if val==self.far:
                 return 'far'
